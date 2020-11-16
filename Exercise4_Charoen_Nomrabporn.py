234740bca7b2da8af6ab1a4d52c589abf495b360
Charoen11 <74525177+Charoen11@users.noreply.github.com>
2020-11-16 13:40:27 +0700

    Create Exercise4_Charoen_Nomrabporn.py

---
+++ Exercise4_Charoen_Nomrabporn.py
@@ -0,0 +1,14 @@
+#FE = Foundation English
+FE = 4.0
+#GB = General Business
+GB = 4.0
+#CS = Introduction to Computer Systems
+CS = 4.0
+#CP = Computer Programming
+CP = 4.0
+
+print("------ Your Score ------")
+print("Foundation English","คะแนนที่ได้", ":", " ", FE)
+print("General Business", "คะแนนที่ได้", ":", " ", GB)
+print("Introduction to Computer System", "คะแนนที่ได้", ":", " ", CS)
+print("Computer Programming", "คะแนนที่ได้", ":", " ", CP)
